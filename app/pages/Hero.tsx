c6f6f1b657632c80174d94b941405c85c10fc769
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-04-27 23:33:49 -0600

    updated Hero Section for mobile screen

--- app/pages/Hero.tsx
+++ app/pages/Hero.tsx
@@ -15,14 +15,19 @@
     return(
       <div
      
-      className="min-h-screen flex flex-1 flex-col gap-7 px-30 py-20">
+      className="min-h-screen flex flex-1 flex-col justify-center gap-7 
+      px-2 py-3
+      xl:px-30 xl:py-20 
+      lg:px-24 lg:py-18
+      md:px-12 md:py-14
+      sm:px-12 sm:py-8
+      ">
           <motion.h3
            initial={{opacity: 0, y:-50}}
       animate={{opacity: 1, y:0}}
       transition={{duration:0.3,ease: "backOut"}} 
-            className="text-xl font-medium  
-                      text-purple-600
-                      "
+            className="text-sm lg:text-2xl md:text-xl sm:text-sm
+            font-medium text-purple-600"
           >
             Hi There, my name is
           </motion.h3>
@@ -31,7 +36,9 @@
           initial={{opacity:0, y: -50}}
           animate={{opacity: 1, y:0}}
           transition={{delay:0.2,duration:0.3, ease:"backOut"}}         
-          className="text-4xl font-extrabold text-shadow-white">Phan Cao Thanh Vinh (Henry)
+          className="
+          text-xl lg:text-4xl md:text-2xl 
+           font-extrabold text-shadow-white">Phan Cao Thanh Vinh (Henry)
           </motion.h1>
           <motion.div
           initial={{opacity:0, y:-50}}
@@ -47,19 +54,25 @@
         ]}
         speed={30}
         repeat={Infinity}
-        className=" text-2xl font-medium text-purple-600"/>
+        className="text-sm lg:text-2xl md:text-xl sm:text-sm
+        font-medium text-purple-600"/>
         </motion.div>
 
         <motion.p
         initial={{opacity: 0, y:-50}}
         animate={{opacity:1, y:0}}
         transition={{delay:0.6, duration:0.3, ease:"backOut"}}
-         className="text-white font-medium text-xl max-w-5xl leading-relaxed space-y-2">
+         className="text-white font-medium 
+         text-xs max-w-sm
+         lg:text-xl lg:max-w-5xl
+         md:text-sm md:max-w-4xl
+         sm:text-xs sm:max-w-3xl 
+         leading-relaxed space-y-2">
     I build applications and websites with modern frameworks, focusing on clean architecture and intuitive user experiences.
     I enjoy tackling complex problems and turning ideas into functional, impactful products.
   </motion.p>
   {/**social icons */}
-    <div className=" flex flex-row gap-5">
+    <div className=" flex flex-row gap-3">
       {socialLinks.map((item :SocialIcons ,index) =>{
         const Icon = item.icon;
         return(
@@ -71,13 +84,13 @@
           href={item.href}
           target="_blank"
           rel="noopener noreferrer">
-          <Icon className="cursor-pointer transition-all duration-300 hover:text-purple-500 hover:-translate-y-2" size={30}/>
+          <Icon className="cursor-pointer transition-all duration-300 hover:text-purple-500 hover:-translate-y-2 lg:text-3xl md:text-2xl sm:text-xl"/>
           </motion.a>
         )
   })}
     </div>
     {/**button */}
-    <div className="flex flex-row gap-7">
+    <div className="flex lg:flex-row flex-col gap-7">
       <motion.button
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
@@ -102,8 +115,12 @@
         ease: "easeOut",
       }}
       className="
-        text-xl font-semibold
-      bg-purple-800 px-5 py-3
+        text-xs px-2 py-1
+        lg:text-xl lg:px-5 lg:py-3
+        md:text-sm md:px-3 md:py-2
+        sm:text-xs sm:px-2 sm:py-1
+        font-semibold
+      bg-purple-800 
       rounded-full cursor-pointer
       hover:opacity-90
       shadow-lg shadow-purple-900/30
@@ -131,11 +148,15 @@
            }}
         className="
           group
-          text-xl font-semibold
+          text-xs px-2 py-1
+          lg:text-xl lg:px-5 lg:py-0
+          md:text-sm md:px-3 md:py-2
+        sm:text-xs sm:px-2 sm:py-1
+          font-semibold
           border-2 border-purple-800 text-purple-800
-          px-5 py-3 rounded-full
+         rounded-full
           cursor-pointer
-          flex items-center gap-2
+          flex items-center justify-center gap-2
           overflow-hidden
         "
       >
